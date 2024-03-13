057cb6c6db97e1e005503c4f1104f0c4d5d45b2a
mhk <matthias@hoelzer-kluepfel.de>
2024-03-13 16:01:22 +0000

    Debug

--- Test/convert-markdown/features/environment.py
+++ Test/convert-markdown/features/environment.py
@@ -6,7 +6,7 @@ from os import path
 from dm.utilities import guessToolsDir
 
 def before_scenario(context, feature):
-    logging.disable(logging.INFO)
+    # logging.disable(logging.INFO)
     context.tmp_html_file = tempfile.TemporaryFile('wb+', suffix='.html')
     context.tmp_tex_file = tempfile.TemporaryFile('w+', encoding='utf-8', suffix='.tex')
     context.tmp_docx_file = tempfile.TemporaryFile('wb+', suffix='.docx')
