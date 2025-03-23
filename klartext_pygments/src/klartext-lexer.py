8692d755c1db132feab7d08376456a728773192f
klartext-dossier <mhk@klartext-dossier.org>
2025-03-23 19:56:11 +0100

    Add pygments lexer for klartext (#6)
    * Add hatch
    
    * Pygments lexer
    
    * Improved lexer
    
    * Add comments
    
    ---------
    
    Co-authored-by: mhk <matthias@hoelzer-kluepfel.de>

---
+++ klartext_pygments/src/klartext-lexer.py
@@ -0,0 +1,38 @@
+"""An example plugin lexer for Pygments."""
+
+import re
+from pygments.lexer import RegexLexer, DelegatingLexer, bygroups, using
+from pygments.token import *
+from pygments.lexers.markup import MarkdownLexer
+
+class KtLexer(RegexLexer):
+
+    name      = "Klartext Markup Language"
+    aliases   = ["klartext", "kt"]
+    filenames = ["*.kt"]
+    mimetypes = ["text/x-klartext"]
+    flags     = re.MULTILINE
+
+    tokens = {
+        'root': [           
+            # directives
+            (r'^([ \t]*)(!include|!import)([ \t]+)(\"[^\"]+\")(?:([ \t]+)(as)([ \t]+)(\w+))?(.*)$', bygroups(Whitespace, Keyword, Whitespace, Comment.PreprocFile, Whitespace, Keyword, Whitespace, Name.Namespace, Whitespace)),
+
+            # tags
+            (r'^([ \t]*)(\w+::)?([\w-]+)(:)([ \t]*)(#[\w\-\.]+)?([ \t]*)(?:([\w\-]+[ \t]*)(=)([ \t]*)(\"[^\"]*\")([ \t]*))*(.*)$', bygroups(Whitespace, Name.Namespace, Name.Tag, Operator, Whitespace, String.Symbol, Whitespace, Name.Attribute, Operator, Whitespace, String, Whitespace, Other)),
+
+            # links
+            (r'^([ \t]*)(\w+::)?([\w\-]+)(>)([ \t]*)([\w\-\.]+)([ \t]*)(?:([\w\-]+[ \t]*)(=)([ \t]*)(\"[^\"]*\")([ \t]*))*(.*)$', bygroups(Whitespace, Name.Namespace, Name.Tag, Operator, Whitespace, String.Symbol, Whitespace, Name.Attribute, Operator, Whitespace, String, Whitespace, Other)),
+
+            # comments
+            (r'^[ \t]*//.*$', Comment),
+
+            # content
+            (r'.*\n', Other)
+        ]
+    }
+
+
+class KlartextLexer(DelegatingLexer):
+    def __init__(self, **options):
+        super().__init__(MarkdownLexer, KtLexer, **options)
