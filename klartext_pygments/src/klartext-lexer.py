763092b5787d0f614ecfecd0e883ccde4b83daee
mhk <matthias@hoelzer-kluepfel.de>
2025-03-23 17:57:23 +0000

    Improved lexer

--- klartext_pygments/src/klartext-lexer.py
+++ klartext_pygments/src/klartext-lexer.py
@@ -1,24 +1,35 @@
 """An example plugin lexer for Pygments."""
 
-from pygments.lexer import RegexLexer, bygroups
+import re
+from pygments.lexer import RegexLexer, DelegatingLexer, bygroups, using
 from pygments.token import *
+from pygments.lexers.markup import MarkdownLexer
 
-class KlartextLexer(RegexLexer):
+class KtLexer(RegexLexer):
 
     name      = "Klartext Markup Language"
     aliases   = ["klartext", "kt"]
     filenames = ["*.kt"]
     mimetypes = ["text/x-klartext"]
+    flags     = re.MULTILINE
 
     tokens = {
         'root': [           
-            # tags
-            (r'^(\s*)(\w+::)?([\w_-]+)(\.[\w_-]+)?(:)(\s*)(#[\w\-_\.]+\s*)?(?:([\w\-_]+\s*)(=)(\s*)(\"[^\"]*\")(\s*))*(.*)$', bygroups(Whitespace, Name.Namespace, Name.Tag, Name.Class, Operator, Whitespace, String.Symbol, Name.Attribute, Operator, Whitespace, String, Whitespace, Text)),
-
             # directives
-            (r'^(\s*)(!)(include|import)(\s+)(\"[^\"]+\")(?:(\s+)(as)(\s+)(\w+))?(.*)$', bygroups(Whitespace, Keyword, Keyword, Whitespace, Comment.PreprocFile, Whitespace, Keyword, Whitespace, Name.Namespace, Whitespace)),
+            (r'^([ \t]*)(!include|!import)([ \t]+)(\"[^\"]+\")(?:([ \t]+)(as)([ \t]+)(\w+))?(.*)$', bygroups(Whitespace, Keyword, Whitespace, Comment.PreprocFile, Whitespace, Keyword, Whitespace, Name.Namespace, Whitespace)),
 
-            # other text
-            (r'.*\n', Text),
+            # tags
+            (r'^([ \t]*)(\w+::)?([\w-]+)(:)([ \t]*)(#[\w\-\.]+)?([ \t]*)(?:([\w\-]+[ \t]*)(=)([ \t]*)(\"[^\"]*\")([ \t]*))*(.*)$', bygroups(Whitespace, Name.Namespace, Name.Tag, Operator, Whitespace, String.Symbol, Whitespace, Name.Attribute, Operator, Whitespace, String, Whitespace, Other)),
+
+            # links
+            (r'^([ \t]*)(\w+::)?([\w\-]+)(>)([ \t]*)([\w\-\.]+)([ \t]*)(?:([\w\-]+[ \t]*)(=)([ \t]*)(\"[^\"]*\")([ \t]*))*(.*)$', bygroups(Whitespace, Name.Namespace, Name.Tag, Operator, Whitespace, String.Symbol, Whitespace, Name.Attribute, Operator, Whitespace, String, Whitespace, Other)),
+
+            # content
+            (r'.*\n', Other)
         ]
     }
+
+
+class KlartextLexer(DelegatingLexer):
+    def __init__(self, **options):
+        super().__init__(MarkdownLexer, KtLexer, **options)
--- klartext_pygments/test.kt
+++ klartext_pygments/test.kt
@@ -1,18 +1,23 @@
 !include "includefile.kt"
 !import "http://www.klartext-dossier.org" as org
 
-single: Some Content
+single: Some _Content_
 
 org::nocontent:
     tag:
         sada
 
+org::link> ID key="value"
+
 one: #ID content 1
     two: #id key="value" anotherkey="anothervalue" content 2
         three: content 3
 
-onemore.class: #ID content 1
+onemore: #ID _content 1_
     twomore: #id key="value" content 2
         threemore: content 3
     
 tag:
+    # Heading
+
+    This *is* simple _markdown_.
