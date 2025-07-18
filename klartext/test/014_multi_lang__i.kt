ecc01885bcf7ffe001d800bc60e0881a5c432aca
mhk <matthias@hoelzer-kluepfel.de>
2025-07-18 11:56:54 +0000

    Add test

---
+++ klartext/test/014_multi_lang__i.kt
@@ -0,0 +1,15 @@
+root: #ID [en]
+
+    german: [de] 
+    german> ID [de] 
+
+    english: [en]
+    english> ID [en]
+
+    british: [en-GB]
+    british> ID [en-GB]
+
+    default:
+    default> ID
+
+    
