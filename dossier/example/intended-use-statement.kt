8465542817080fc0e1d02629094c8c7578315e2d
mhk <matthias@hoelzer-kluepfel.de>
2025-08-27 09:29:09 +0000

    Fix

--- dossier/example/intended-use-statement.kt
+++ dossier/example/intended-use-statement.kt
@@ -5,13 +5,13 @@
 md::intended-use: #gds-intended-use
 
     md::intended-use-summary:
-        The /r/device/ is a dialysis machine providing haemodialysis treatments for /r/patient/s with permanent kidney failure.
+        The /r/device/ is a {dialysis machine} providing {haemodialysis} treatments for /r/patient/s with {permanent kidney failure}.
 
     md::medical-purpose:
-        The /r/device/ provides a renal replacement therapy with the following clinical functions:
+        The /r/device/ provides a renal replacement therapy with the following {clinical functions}:
         
         md::clinical-function: name="Ultrafiltration"
-            Removal of substances with low molecular weight from the /r/patient/'s blood
+            Removal of substances with low {molecular weight} from the /r/patient/'s blood
 
         md::clinical-function: name="Net Fluid Removal"
             Removal of excess water from the /r/patient/'s blood
@@ -19,13 +19,13 @@ md::intended-use: #gds-intended-use
         ops::ops-code> ops::code-8-854.x
 
     md::medical-indication:
-        /r/patient/s with permanent kidney failure and low glomerular filtration rate (GFR < 15 mL/min).
+        /r/patient/s with {permanent kidney failure} and low {glomerular filtration rate} (GFR < 15 mL/min).
 
         icd::icd-code> icd::code-N18.5
         icd::icd-code> icd::code-Z99.2
         
     md::patient-group: #patient name="Patient"
-        Patients with permanent kidney failure and a body-weight > 40 kg.
+        Patients with {permanent kidney failure} and a {body-weight} > 40 kg.
 
     md::intended-user: #physician name="Nephrologist"
 
